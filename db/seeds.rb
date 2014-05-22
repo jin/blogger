c5dfc1dffb9bb70111e56b74f731a791d2ba527c
jin <jin@crypt.sg>
2014-05-22 23:44:33 +0800

    Increased the number of paragraphs for article seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ require 'faker'
 
 3.times do 
   title = Faker::Lorem.sentence
-  body = Faker::Lorem.paragraph(1, true, 3)
+  body = Faker::Lorem.paragraph(50, true, 100)
   Article.create(title: title, body: body)
 end
