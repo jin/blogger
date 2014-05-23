b0ffed87ef3c749eda4ad04251cf72c422b8b8d9
jin <jin@crypt.sg>
2014-05-23 20:03:37 +0800

    Added the delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,8 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article.id)
   end
 
+  def destroy
+    Article.find(params[:id]).destroy! 
+    redirect_to articles_path
+  end
 end
