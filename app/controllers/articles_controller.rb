d273ff0c63de4dc402e71e1973e1e23242965b2a
jin <jin@crypt.sg>
2014-05-23 18:31:48 +0800

    Moved navbar code into a partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all 
+    @page_title = "All articles"
   end
 
   def show
     @article = Article.find(params[:id]) 
+    @page_title = @article.title
   end
 
   def new
