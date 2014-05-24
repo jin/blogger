99b15fa3d03ef2ef1c6e51dfc8286f8cc066a7a9
jin <jin@crypt.sg>
2014-05-24 19:11:28 +0800

    Added a simple flash notice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,16 +24,25 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])    
     @article.update(article_params)
+
+    flash.notice = "Successfully updated article."
+
     redirect_to article_path(@article.id)
   end
 
   def create
     @article = Article.create(article_params)
+
+    flash.notice = "Successfully created article."
+
     redirect_to article_path(@article.id)
   end
 
   def destroy
     Article.find(params[:id]).destroy! 
+
+    flash.notice = "Sucessfully deleted article."
+
     redirect_to articles_path
   end
 end
